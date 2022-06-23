185567305a2ae13641ff4651e8578285bcf2608d
Andrea Colucci <andreacolucci1994@gmail.com>
2022-06-23 10:25:28 +0200

    Sql exercise on table

---
+++ SQL_Table/src/main/java/Connector.java
@@ -0,0 +1,62 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Connector {
+
+    private Connection conn = null;
+    private boolean doesTabExist = false;
+    private int id = 0; //Contatore degli id
+
+    public Connection connection() throws SQLException, ClassNotFoundException {
+        if(conn == null){
+            // db parameters
+            String url       = "jdbc:mysql://localhost:3306/newdb";
+            String user      = "developer";
+            String password  = "Developerpass1!";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");//Nome della connessione
+            conn = DriverManager.getConnection(url,user,password);
+
+            System.out.println("Connessione avvenuta con successo!");
+        }
+        return conn;
+    }
+
+    public void inserisciUtente(UtenteDB utente) throws SQLException{
+        if(conn == null || !doesTabExist){
+            System.out.println("Usa il comando 'connection()' per effettuare la connessione e crea la tabella se non esiste!");
+        }
+        else{
+            String query = "INSERT INTO students(User_ID, Name, Surname) VALUES (?,?,?);";
+            //I punti di domanda sono necessari per prevenire sql injection
+
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            //Inserisco lo statement contenuto dentro 'query'
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2,utente.getName());
+            preparedStatement.setString(3,utente.getSurname());
+            //Modifico i valori, prima segno nella riga quale colonna modificare, poi con cosa la modifico
+            preparedStatement.executeUpdate();//Esegue le modifiche che ho apportato
+            id++;
+            System.out.println("User " + utente.getName() + " " + utente.getSurname() + " inserito con successo.");
+        }
+    }
+
+    public void creaTabella() throws SQLException{
+        if(conn == null){
+            System.out.println("Usa il comando 'connection()' per effettuare la connessione prima!");
+        }
+        else{
+            doesTabExist = true;
+            String query = "CREATE TABLE IF NOT EXISTS students (User_ID INT NOT NULL AUTO_INCREMENT, " +
+                    " Name VARCHAR(50), " +
+                    "Surname VARCHAR(50)" +
+                    ", PRIMARY KEY(User_ID));";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            System.out.println("La tabella 'students' è stata creata con successo.");
+        }
+    }
+}
